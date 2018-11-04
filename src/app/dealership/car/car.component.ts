efc9c4cbeb9dfa374ccc0e5e08722c00898f4e84
Momchil Iliev <momchil.s.iliev@gmail.com>
2018-11-04 18:33:33 +0200

    card-body

---
+++ src/app/dealership/car/car.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'car',
+	templateUrl: './car.component.html'
+})
+
+export class CarComponent implements OnInit {
+
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
